bc6be3a0bd81be2eceda80097d5545b4fc71b5db
MayKikuchi <m.may.kikuchi@gmail.com>
2021-02-01 21:00:29 -0800

    changes to get app working in AS and SignUp screen

--- neighbouri/App.js
+++ neighbouri/App.js
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProfileScreen from './src/screens/Profile';
+import SignUpScreen from './src/screens/SignUp';
 
 function HomeScreen() {
   return (
@@ -26,6 +27,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="SignUp" component={SignUpScreen} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
