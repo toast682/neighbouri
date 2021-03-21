d1786eca0b002e84f0db71a3c8b7dc303b6f2977
Adrian <88adrianlee@gmail.com>
2021-03-21 16:42:26 -0700

    users are able to change usernames and add/change bio

--- neighbouri/src/screens/Profile.js
+++ neighbouri/src/screens/Profile.js
@@ -1,23 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {
-  Text,
-  View,
-  FlatList,
-  Image,
-  StyleSheet,
-  Modal,
-  TouchableOpacity,
-} from 'react-native';
+import {Text, View, FlatList, Image, StyleSheet, Modal, TouchableOpacity} from 'react-native';
 import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import {HPageViewHoc} from 'react-native-head-tab-view';
 import {CollapsibleHeaderTabView} from 'react-native-scrollable-tab-view-collapsible-header';
+import HistoryButton from '../components/HistoryButton';
 import Header from '../components/navigation/Header';
 import SettingButton from '../components/SettingButton';
+import GeoButton from '../components/GeoButton';
+import ProfileIconButton from '../components/ProfileIconButton';
 import UserInfoText from '../components/profile/UserInfoText';
-import {Rating} from 'react-native-ratings';
-import moment from 'moment'
+import { Rating } from 'react-native-ratings';
+
 
 export default function ProfileScreen({navigation}) {
   const HFlatList = HPageViewHoc(FlatList);
@@ -26,54 +21,11 @@ export default function ProfileScreen({navigation}) {
   const [user, setUser] = useState();
   const [show, setShow] = useState(false);
   const [currRating, setCurrRating] = useState(0);
-  const [listings, setListings] = useState([]);
-  const [purchases, setPurchases] = useState([]);
+
 
   useEffect(() => {
     getData();
-    getListingData();
-    getPurchaseData();
   }, []);
-
-  async function getListingData() {
-    setListings([]);
-    await firestore()
-      .collection('Listings')
-      .where('SellerID', '==', auth().currentUser.uid)
-      .orderBy('PostedDate', 'desc')
-      .get()
-      .then((listingDocs) => {
-        listingDocs.forEach((doc) => {
-          buildObject(doc);
-        });
-      });
-  }
-
-  async function getPurchaseData() {
-    setPurchases([]);
-    await firestore()
-      .collection('Transactions')
-      .where('BuyerID', '==', auth().currentUser.uid)
-      .orderBy('Date', 'desc')
-      .get()
-      .then((purchaseDocs) => {
-        purchaseDocs.forEach((doc) => {
-          buildPurchase(doc);
-        });
-      });
-  }
-
-  async function buildObject(doc) {
-    const reference = await storage().ref(doc.data().ImageURI).getDownloadURL();
-    doc.data().photo = {uri: reference};
-    setListings((prev) => [...prev, doc.data()]);
-  }
-
-  async function buildPurchase(doc) {
-    const reference = await storage().ref(doc.data().ImageURI).getDownloadURL();
-    doc.data().photo = {uri: reference};
-    setPurchases((prev) => [...prev, doc.data()]);
-  }
 
   function showRating(rating) {
     setCurrRating(rating);
@@ -82,12 +34,8 @@ export default function ProfileScreen({navigation}) {
 
   async function getData() {
     const reference = await storage()
-      .ref('ProfilePicture')
-      .child(auth().currentUser.uid + '.JPG')
-      .getDownloadURL()
-      .catch(() => {
-        storage().ref('ProfilePicture').child('Apple.jpg').getDownloadURL();
-      });
+      .ref('ProfilePicture/Apple.jpg')
+      .getDownloadURL();
     setPhoto(reference);
 
     await firestore()
@@ -105,12 +53,7 @@ export default function ProfileScreen({navigation}) {
 
   return (
     <View
-      style={{
-        height: '100%',
-        backgroundColor: 'white',
-        flex: 1,
-        paddingTop: 30,
-      }}>
+      style={{height: '100%', backgroundColor: 'white', flex: 1, paddingTop: 30 }}>
       {Header(
         <View style={{flex: 1}} />,
         <View style={{flex: 5}} />,
@@ -118,8 +61,6 @@ export default function ProfileScreen({navigation}) {
       )}
       <CollapsibleHeaderTabView
         makeHeaderHeight={() => 120}
-        tabBarActiveTextColor='#48CA36'
-        tabBarUnderlineStyle={{backgroundColor: '#48CA36'}}
         renderScrollHeader={() => (
           <View
             style={{
@@ -150,24 +91,23 @@ export default function ProfileScreen({navigation}) {
                 {user.Username}
               </Text>
               <Rating
-                style={{marginBottom: 10, alignSelf: 'flex-start'}}
-                startingValue={user.SellerRating[1]}
-                readonly={true}
-                imageSize={25}
-                ratingCount={5}
+                  startingValue={user.SellerRating[1]}
+                  readonly={true}
+                  imageSize={25}
+                  ratingCount={5}
               />
-              {UserInfoText(require('../assets/Phone.png'), user.Phone)}
+              {UserInfoText(require('../assets/History.png'), user.Phone)}
             </View>
           </View>
         )}>
         <HFlatList
           index={0}
           tabLabel={'Listings'}
-          data={[...new Set(listings)]}
+          data={['Banana', 'Pizza', 'Apples', 'Milk', 'Ramen']}
           renderItem={({item}) => (
             <View
               style={{
-                backgroundColor: '#faf9f9',
+                backgroundColor: 'grey',
                 margin: 10,
                 height: 150,
                 borderRadius: 8,
@@ -179,15 +119,13 @@ export default function ProfileScreen({navigation}) {
                   justifyContent: 'space-between',
                   margin: 10,
                 }}>
-                <Text>Listed on {moment(item.PostedDate.toDate(), 'YYYYMMDD').format('ll')}</Text>
-                <Text style={{color: 'brown'}} onPress={() => {
-                  console.log('edit')
-                }}>Edit Post</Text>
+                <Text>Purchased on date</Text>
+                <Text>View Receipt</Text>
               </View>
               <View style={{flex: 3, flexDirection: 'row'}}>
                 <View>
                   <Image
-                    source={{uri: item.photo.uri}}
+                    source={{uri: photo}}
                     style={{
                       width: 80,
                       height: 80,
@@ -196,13 +134,8 @@ export default function ProfileScreen({navigation}) {
                     }}></Image>
                 </View>
                 <View>
-                  <Text style={{fontWeight: 'bold'}}>{item.Item}</Text>
-                  <Text>
-                    ${item.Price}
-                  </Text>
-                  <Text>
-                    {item.Description}
-                  </Text>
+                  <Text>{item}</Text>
+                  <Text onPress={()=>{showRating(3)}}>Review Seller</Text>
                 </View>
               </View>
             </View>
@@ -212,12 +145,12 @@ export default function ProfileScreen({navigation}) {
         />
         <HFlatList
           index={1}
-          data={[...new Set(purchases)]}
+          data={['B', 'P', 'A']}
           tabLabel={'Purchases'}
           renderItem={({item}) => (
             <View
               style={{
-                backgroundColor: '#faf9f9',
+                backgroundColor: 'grey',
                 margin: 10,
                 height: 150,
                 borderRadius: 8,
@@ -229,26 +162,23 @@ export default function ProfileScreen({navigation}) {
                   justifyContent: 'space-between',
                   margin: 10,
                 }}>
-                <Text>Purchased on {moment(item.Date.toDate(), 'YYYYMMDD').format('ll')}</Text>
-                <Text style={{color: 'brown'}} onPress={() => {
-                  console.log('receipt')
-                }}>View Receipt</Text>
+                <Text>Purchased on date</Text>
+                <Text>View Receipt</Text>
               </View>
               <View style={{flex: 3, flexDirection: 'row'}}>
                 <View>
                   <Image
-                    source={{uri: item.photo.uri}}
+                    source={{uri: photo}}
                     style={{
                       width: 80,
                       height: 80,
                       borderRadius: 8,
                       marginHorizontal: 10,
-                    }}/>
+                    }}></Image>
                 </View>
                 <View>
-                  <Text style={{fontWeight: 'bold'}}>{item.Item} - ${item.Price}</Text>
-                  <Text>{item.SellerName}</Text>
-                  <Text style={{color: '#F9A528', textDecorationLine: 'underline'}}>Review Seller</Text>
+                  <Text>{item}</Text>
+                  <Text>Review Seller</Text>
                 </View>
               </View>
             </View>
@@ -256,10 +186,54 @@ export default function ProfileScreen({navigation}) {
           style={{width: '100%'}}
           keyExtractor={(name) => name}
         />
-      </CollapsibleHeaderTabView>
+        <HFlatList
+          index={2}
+          data={['B', 'P', 'A']}
+          tabLabel={'Reviews'}
+          renderItem={({item}) => (
+            <View
+              style={{
+                backgroundColor: 'grey',
+                margin: 10,
+                height: 150,
+                borderRadius: 8,
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  margin: 10,
+                }}>
+                <Text>Purchased on date</Text>
+                <Text>View Receipt</Text>
+              </View>
+              <View style={{flex: 3, flexDirection: 'row'}}>
+                <View>
+                  <Image
+                    source={{uri: photo}}
+                    style={{
+                      width: 80,
+                      height: 80,
+                      borderRadius: 8,
+                      marginHorizontal: 10,
+                    }}></Image>
+                </View>
+                <View>
+                  <Text>{item}</Text>
+                  <Text>Review Seller</Text>
+                </View>
+              </View>
+            </View>
+          )}
+          style={{width: '100%'}}
+          keyExtractor={(name) => name}
+        />
+      </CollapsibleHeaderTabView>  
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
