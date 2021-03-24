5240fd5685498fec75fe4db6bd2a4d29c3d1bfe3
Amogh Joshi <amoghjoshi24@gmail.com>
2021-03-24 00:49:12 -0600

    added chat things

--- neighbouri/App.js
+++ neighbouri/App.js
@@ -1,14 +1,13 @@
-
 import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
-import { Text, View, Platform, Image } from 'react-native';
-import { enableScreens } from 'react-native-screens';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, {useState, useEffect} from 'react';
+import {Text, View, Platform, Image} from 'react-native';
+import {enableScreens} from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import auth from '@react-native-firebase/auth';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProfileScreen from './src/screens/Profile';
 import HistoryScreen from './src/screens/History';
 import LoginScreen from './src/screens/LogIn';
@@ -17,6 +16,7 @@ import HomeScreen from './src/screens/Home';
 import ProfileInfoScreen from './src/screens/ProfileInfo';
 import CreatePostingScreen from './src/screens/CreatePosting';
 import ListingDetailsScreen from './src/screens/ListingDetails';
+import ChatScreen from './src/screens/ChatScreen';
 
 enableScreens();
 
@@ -24,20 +24,23 @@ const HomeStack = createStackNavigator();
 
 function Home() {
   return (
-        <HomeStack.Navigator
-        initialRouteName='Home'
-        screenOptions={{
-          headerShown: false,
-        }}>
-          <HomeStack.Screen name='Home' component={HomeScreen} />
-          <HomeStack.Screen name='ListingDetails' component={ListingDetailsScreen} />
-        </HomeStack.Navigator>
-  )
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen
+        name="ListingDetails"
+        component={ListingDetailsScreen}
+      />
+    </HomeStack.Navigator>
+  );
 }
 
 function SavedScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Saved!</Text>
     </View>
   );
@@ -46,38 +49,36 @@ function SavedScreen() {
 function CreateScreen() {
   return (
     <HomeStack.Navigator
-        initialRouteName='CreatePosting'
-        screenOptions={{
-          headerShown: false,
-        }}>
-          <HomeStack.Screen name='CreatePosting' component={CreatePostingScreen} />
-        </HomeStack.Navigator>
+      initialRouteName="CreatePosting"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <HomeStack.Screen name="CreatePosting" component={CreatePostingScreen} />
+    </HomeStack.Navigator>
   );
 }
 
-function FeedScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
-    </View>
-  );
-}
-
+// function FeedScreen() {
+//   return (
+//     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//       <Text>Feed!</Text>
+//     </View>
+//   );
+// }
 
 const ProfileStack = createNativeStackNavigator();
 function Profile() {
   return (
     <ProfileStack.Navigator
-    initialRouteName="Profile"
-    screenOptions={{
-      headerShown: false,
-    }}
-    >
+      initialRouteName="Profile"
+      screenOptions={{
+        headerShown: false,
+      }}>
       <ProfileStack.Screen name="Profile" component={ProfileScreen} />
       <ProfileStack.Screen name="History" component={HistoryScreen} />
       <ProfileStack.Screen name="ProfileInfo" component={ProfileInfoScreen} />
     </ProfileStack.Navigator>
-  )
+  );
 }
 
 const SignUpLoginStack = createNativeStackNavigator();
@@ -105,7 +106,9 @@ export default function App() {
   if (!user) {
     return (
       <NavigationContainer>
-        <SignUpLoginStack.Navigator initialRouteName="SignUpScreen" screenOptions={{headerShown: false}}>
+        <SignUpLoginStack.Navigator
+          initialRouteName="SignUpScreen"
+          screenOptions={{headerShown: false}}>
           <SignUpLoginStack.Screen name="SignUp" component={SignUpScreen} />
           <SignUpLoginStack.Screen name="LogIn" component={LoginScreen} />
         </SignUpLoginStack.Navigator>
@@ -116,14 +119,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen 
-          name="Home" 
+        <Tab.Screen
+          name="Home"
           component={Home}
           options={{
             tabBarLabel: '',
             tabBarIcon: ({focused}) => (
               <Image
-                source={focused ? require('./src/assets/HomeF.png') : require('./src/assets/Home.png')}
+                source={
+                  focused
+                    ? require('./src/assets/HomeF.png')
+                    : require('./src/assets/Home.png')
+                }
                 style={{
                   width: '60%',
                   height: '60%',
@@ -132,15 +139,20 @@ export default function App() {
                 }}
               />
             ),
-          }} />
-          <Tab.Screen 
-          name="Saved" 
+          }}
+        />
+        <Tab.Screen
+          name="Saved"
           component={SavedScreen}
           options={{
             tabBarLabel: '',
             tabBarIcon: ({focused}) => (
               <Image
-                source={focused ? require('./src/assets/SavedF.png') : require('./src/assets/Saved.png')}
+                source={
+                  focused
+                    ? require('./src/assets/SavedF.png')
+                    : require('./src/assets/Saved.png')
+                }
                 style={{
                   width: '60%',
                   height: '60%',
@@ -149,9 +161,10 @@ export default function App() {
                 }}
               />
             ),
-          }} />
-        <Tab.Screen 
-          name="Create" 
+          }}
+        />
+        <Tab.Screen
+          name="Create"
           component={CreateScreen}
           options={{
             tabBarLabel: '',
@@ -166,15 +179,20 @@ export default function App() {
                 }}
               />
             ),
-          }} />
-          <Tab.Screen 
-          name="Feed" 
-          component={FeedScreen}
+          }}
+        />
+        <Tab.Screen
+          name="Feed"
+          component={ChatScreen}
           options={{
             tabBarLabel: '',
             tabBarIcon: ({focused}) => (
               <Image
-                source={focused ? require('./src/assets/NotificationF.png') : require('./src/assets/Notification.png')}
+                source={
+                  focused
+                    ? require('./src/assets/NotificationF.png')
+                    : require('./src/assets/Notification.png')
+                }
                 style={{
                   width: '60%',
                   height: '60%',
@@ -183,16 +201,21 @@ export default function App() {
                 }}
               />
             ),
-          }} />
-        <Tab.Screen 
-          name="Profile" 
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
           component={Profile}
           options={{
             tabBarLabel: '',
             tabBarIcon: ({focused}) => (
               <Image
-              source={focused ? require('./src/assets/ProfileF.png') : require('./src/assets/Profile.png')}
-              style={{
+                source={
+                  focused
+                    ? require('./src/assets/ProfileF.png')
+                    : require('./src/assets/Profile.png')
+                }
+                style={{
                   width: '60%',
                   height: '60%',
                   resizeMode: 'contain',
@@ -200,9 +223,9 @@ export default function App() {
                 }}
               />
             ),
-          }} />
+          }}
+        />
       </Tab.Navigator>
-
     </NavigationContainer>
   );
 }
---
+++ neighbouri/src/screens/ChatScreen.js
@@ -0,0 +1,65 @@
+import React, {useState, useCallback, useEffect} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+import firestone from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+
+const chat = firestone().collection('Chat');
+
+export default function ChatScreen() {
+  const [messages, setMessages] = useState([]);
+  const currentUserId = auth().currentUser;
+
+  //   useEffect(() => {
+  //     getPreviousMessages();
+  //   }, []);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  async function getPreviousMessages() {
+    // await messageStorage
+    //   .where('uid', '==', currentUserId)
+    //   .get()
+    //   .then((messages) => {
+    //     setMessages([messages]);
+    //   })
+    //   .catch((e) => {
+    //     console.log('You fucked up');
+    //   });
+  }
+
+  const upDateFirestone = async (messages) => {
+    chat.add(messages);
+  };
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+    upDateFirestone(messages).then(() => {
+      console.log('success');
+    });
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: currentUserId,
+      }}
+    />
+  );
+}
