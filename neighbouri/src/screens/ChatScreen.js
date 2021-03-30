27e2230590ffabe4329afe9e24f27cc469a4ab40
Adrian <88adrianlee@gmail.com>
2021-03-29 22:58:27 -0700

    restructure chats to make more sense

--- neighbouri/src/screens/ChatScreen.js
+++ neighbouri/src/screens/ChatScreen.js
@@ -8,57 +8,12 @@ const chat = firestore().collection('Chats');
 export default function ChatScreen() {
   const [messages, setMessages] = useState([]);
   const currentUserId = auth().currentUser.uid;
+  const currentUser = auth().currentUser;
 
   useEffect(() => {
     getPreviousMessages();
-    console.log(currentUserId);
-    console.log(auth().currentUser.displayName);
-  }, []);
+  }, [getPreviousMessages, messages]);
 
-  // useEffect(() => {
-  // fetch messages from conversation between current user and user they are talking to.
-  // setMessages([
-  //   {
-  //     _id: 1,    // Message Id
-  //     text: 'Hello developer', // Text of Message
-  //     createdAt: new Date(), // Timestamp of when message was created
-  //     user: {
-  //       _id: 2,              // UserID of person who sent the message
-  //       name: 'React Native',   // Name of the User
-  //       avatar: 'https://placeimg.com/140/140/any',    // Link of User Profile Picture
-  //     },
-  //   },
-  //   {
-  //     _id: 2321,
-  //     text: 'Hello developer',
-  //     createdAt: new Date(),
-  //     user: {
-  //       _id: 2,
-  //       name: 'React Native',
-  //       avatar: 'https://placeimg.com/140/140/any',
-  //     },
-  //   },
-  //   {
-  //     _id: 2,
-  //     text: 'Hello',
-  //     createdAt: new Date(),
-  //     user: {
-  //       _id: currentUserId,
-  //       name: 'React Native',
-  //       avatar: 'https://placeimg.com/140/140/any',
-  //     },
-  //   },
-  //   {
-  //     _id: 6969,
-  //     text: 'Hello',
-  //     createdAt: new Date(),
-  //     user: {
-  //       _id: currentUserId,
-  //       name: 'React Native',
-  //       avatar: 'https://placeimg.com/140/140/any',
-  //     },
-  //   },
-  // ]);
   // Esteban Codes Message
   //   const unsubscribe = chat.onSnapshot((querySnapshot) => {
   //     const messagesFirestore = querySnapshot
@@ -73,24 +28,15 @@ export default function ChatScreen() {
   //   });
   //   return () => unsubscribe();
   // }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   async function getPreviousMessages() {
-    const chatsWhereUser1IsCurrentUser = await chat
-      .where('User1', '==', currentUserId)
+    const userChat = await chat
+      .where('chatMembers', 'array-contains', currentUserId)
       .get();
-    // .then(() => {
-    //   setMessages(chatsWhereUser1IsCurrentUser.docs.messages);
-    // });
-    // const chatsWhereUser2IsCurrentUser = await chat
-    //   .where('User2', '==', currentUserId)
-    //   .get();
-    // const messageArrayUser2 = chatsWhereUser2IsCurrentUser.docs;
-
-    //Note that we don't need to de-duplicate in this case
-    // chatsWhereUser1IsCurrentUser._docs[0]._data.messages.map((message) =>
-    //   message.messages.createdAt.toDate(),
-    // );
-    chatsWhereUser1IsCurrentUser._docs[0]._data.messages[0].createdAt = chatsWhereUser1IsCurrentUser._docs[0]._data.messages[0].createdAt.toDate();
-    setMessages(chatsWhereUser1IsCurrentUser._docs[0]._data.messages);
+    userChat._docs[0]._data.messages.map(
+      (message) => (message.createdAt = message.createdAt.toDate()),
+    );
+    setMessages(userChat._docs[0]._data.messages);
   }
 
   //   useEffect(() => {
@@ -111,26 +57,32 @@ export default function ChatScreen() {
   //     return () => unsubscribe()
   // }, [])
 
-  // async function getPreviousMessages() {
-  //   await chat
-  //     .where('User1', '==', currentUserId)
-  //     .get()
-  //     .then((messages) => {
-  //       setMessages([messages]);
-  //     })
-  //     .catch((e) => {
-  //       console.log('No messages found');
-  //     });
-  // }
 
+
+
+  // const onSend = useCallback((messages = []) => {
+  //   setMessages((previousMessages) =>
+  //     GiftedChat.append(previousMessages, messages),
+  //   );
+  // }, []);
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => onSend(messages)}
+      onSend={async (messages) => {
+
+        // const userChat = await chat
+        //   .where('chatMembers', 'array-contains', currentUserId)
+        //   .get();
+        // const userChatMessages = userChat._docs[0]._data.messages;
+        // userChatMessages.add(messages[0]);
+        // console.log(messages);
+        // console.log(userChat);
+        // console.log(userChatMessages);
+      }}
       user={{
         _id: currentUserId,
-        name: 'React Native',
-        avatar: 'https://placeimg.com/140/140/any',
+        name: currentUser.displayName ? currentUser.displayName : '',
+        avatar: currentUser.photoURL ? currentUser.photoURL : '',
       }}
     />
   );
